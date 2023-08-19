a84cd7769c021a5fa6a836acda6fc945ab6e4444
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-08-19 22:14:50 +0300

    [Backend] Setting Up Basic Auth Routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use PharIo\Manifest\AuthorCollectionIterator;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::get('logout', [AuthController::class, 'logout']);
 });
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('unauthorized', [AuthController::class, 'unauthorized'])->name('unauthorized');
