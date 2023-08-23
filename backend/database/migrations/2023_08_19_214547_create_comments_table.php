8a303b819d2905ea2a639f5bd2ed1c50dfe20548
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-08-23 11:29:22 +0300

    [Backend] Created Comments Table Migration

--- backend/database/migrations/2023_08_19_214547_create_comments_table.php
+++ backend/database/migrations/2023_08_19_214547_create_comments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('recipe_id');
-            $table->mediumText('conent');
+            $table->mediumText('content');
             $table->timestamps();
         });
     }
