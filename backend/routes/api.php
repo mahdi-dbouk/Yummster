6e883dd62069201c9016d360bd4a1ee0c301dcde
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-08-23 11:51:24 +0300

    [Backend] Added Routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\GroceryListController;
+use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use PharIo\Manifest\AuthorCollectionIterator;
@@ -17,10 +21,21 @@ use PharIo\Manifest\AuthorCollectionIterator;
 */
 
 Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('create-recipe', [RecipeController::class, 'createARecipe']);
+    Route::post('create-grocery-list', [GroceryListController::class, 'createAGroceryList']);
+    Route::post('add-comment', [CommentController::class, 'addComment']);
+    Route::post('like', [RecipeController::class, 'like']);
+    Route::post('unlike', [RecipeController::class, 'unlike']);
+    Route::post('rate', [RecipeController::class, 'rate']);
+    Route::get('profile/{id}',[UserController::class, 'getProfile']);
+    Route::get('recipe/{id}', [RecipeController::class, 'getRecipe']);
+    Route::get('search/{search_query}', [RecipeController::class, 'search']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('logout', [AuthController::class, 'logout']);
 });
 
 Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::post('unauthorized', [AuthController::class, 'unauthorized'])->name('unauthorized');
+Route::group(['middleware'=>'throttle:login'], function(){
+    Route::post('login', [AuthController::class, 'login']);
+});
+Route::get('unauthorized', [AuthController::class, 'unauthorized'])->name('unauthorized');
