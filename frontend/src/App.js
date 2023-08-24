1d7f370415fe5d7dc2f4f1b7b34c745b26db973e
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-08-24 19:33:57 +0300

    [Frontend] Implemented Routes using React-Router-Dom Part 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import RootLayout from './layouts/RootLayout';
+import Feed from './layouts/FeedLayout';
+import Landing from './layouts/LandingLayout';
+import Planner from './layouts/Planner';
+import GroceryList from './layouts/GroceryList';
+import Recipe from './layouts/RecipeLayout';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route index element={<Landing />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/recipe' element={<Recipe />} />
+          <Route path='/planner' element={<Planner />} />
+          <Route path='/grocery-list' element={<GroceryList />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
